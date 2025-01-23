4a9ce439eb2778c38624bfa8dd7d038ee74f93f0
emmawirts <emmaroselee14@gmail.com>
2025-01-23 17:30:13 -0500

    Created script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+// Selectors
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// Event Listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('input', setVideoProgress);
+
+// Handlers
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+function updateProgress() {
+  const percentage = (video.currentTime / video.duration) * 100;
+  progress.value = percentage;
